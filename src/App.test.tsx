56e35ec2a2e0c3f356f28b4e3626af551c521cca
doehlercavalcanti-12 <doehlercavalcanti@gmail.com>
2025-10-30 16:46:35 -0300

    Update heading test to check for 'test' instead of 'about genauxi'

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -15,7 +15,7 @@ describe('App', () => {
   it('renders hero heading and call to action', () => {
     render(<App />);
 
-    expect(screen.getByRole('heading', { name: /about genauxi/i })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /test/i })).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /get started/i })).toBeInTheDocument();
   });
 });
